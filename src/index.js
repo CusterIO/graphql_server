81ee3156c6538e03d927b68ec7f77f7e43c81803
CusterIO <custer.app@gmail.com>
2019-05-18 16:13:29 +0200

    add session and cors for security

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { prisma } = require('../prisma/src/generated/prisma-client')
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const User = require('./resolvers/User')
-const Link = require('./resolvers/Link')
-const Subscription = require('./resolvers/Subscription')
-const Vote = require('./resolvers/Vote')
-const Article = require('./resolvers/Article')
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("../prisma/src/generated/prisma-client");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
+const Article = require("./resolvers/Article");
+const { FRONTEND_URL, SESSION_SECRET, MODE } = require("../config/config.js");
+const session = require("express-session");
 
 const resolvers = {
   Query,
@@ -16,17 +18,43 @@ const resolvers = {
   Link,
   Vote,
   Article
-}
+};
 
 const server = new GraphQLServer({
-  typeDefs: 'src/schema.graphql',
+  typeDefs: "src/schema.graphql",
   resolvers,
   context: request => {
     return {
       ...request,
       prisma
-    }
+    };
   }
-})
+});
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.express.use(
+  session({
+    name: "CusterIO",
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: MODE === "production",
+      maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+    }
+  })
+);
+
+const serverOptions = {
+  port: 4000,
+  cors: {
+    credentials: true,
+    origin: [FRONTEND_URL]
+  }
+};
+
+server.start(serverOptions, () => {
+  console.log(
+    `Server is now running on port http://localhost:${serverOptions.port}`
+  );
+});
